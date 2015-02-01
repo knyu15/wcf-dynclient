72a81fccd5107a472c6231aa8edf69e18bc9f2f4
knyu <knyu@gmail.com>
2015-02-01 17:04:31 +0300

    Comments added

--- wcf_dynclient_proxy_client/Program.cs
+++ wcf_dynclient_proxy_client/Program.cs
@@ -9,7 +9,10 @@ namespace wcf_dynclient_proxy_client
 {
     class Program
     {
+        // persistent proxy object
         private static readonly Proxy Proxy = new Proxy("http://localhost:8100/mex", "IDynClientLib");
+        
+        // persistent dynamic client-side instance of the WCF service contract implementation
         private static dynamic Instance;
 
         static void Main(string[] args)
@@ -20,21 +23,33 @@ namespace wcf_dynclient_proxy_client
 
         private static void Demo(string testString)
         {
+            // Check if we have an instance, create otherwise
             if (Instance == null)
                 Instance = Proxy.CreateNewInstance();
 
+            // Check if update of contract needed. 
+            // Please note, that if contract was updated (some methods added, some removed, etc.) the
+            // existent code may be not valid!
             if (Proxy.IsUpdateNeeded())
                 Instance = Proxy.CreateNewInstance();
 
+            // Get response of the server (HashInfo instance)
             var serverHash = Instance.GetHash(testString);
+            
+            // Server-side hash bytes
             var serverHashBytes = serverHash.Hash;
+
+            // Client-side hash bytes
             var clientHashBytes = GetHash(testString);
 
+            // Check if server returns correct hash algorithm info
             if (serverHash.HashAlgorithm != "MD5")
                 throw new InvalidOperationException("Wrong hash algorithm");
 
+            // Dump algorithm info to console
             Console.WriteLine(serverHash.HashAlgorithm);
 
+            // Check if hash valid
             if (CompareHash(serverHashBytes, clientHashBytes) == false)
                 throw new InvalidOperationException("Hash does not equals");
             else
